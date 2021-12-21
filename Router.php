1931bfb6a2fe83fd5772f7f11ea5481c9c42a5da
FrankfurtAm <bobkov-03@mail.ru>
2021-12-21 15:04:24 +0600

    Router

---
+++ Router.php
@@ -0,0 +1,24 @@
+<?php 
+
+class Router
+{
+    private $routes;
+
+    public function __construct(array $routes)
+    {
+        $this->routes = $routes;
+    }
+
+    public function route(string $route)
+    {
+        if(!array_key_exists($route, $this->routes)) return $this->display404();
+
+        require '../app/models/'.$this->routes[$route].'.model.php';
+        require '../app/views/'.$this->routes[$route].'.view.php';
+    }
+
+    private function display404()
+    {
+        require('../app/views/404.php');
+    }
+}
